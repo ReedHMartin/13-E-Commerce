a1072af699a4fb3b71a6c3faea30d5a20a0e1c90
Reed Martin <reedhmartin@gmail.com>
2023-04-12 18:07:44 -0400

    Added to category.init.

--- models/Category.js
+++ models/Category.js
@@ -11,6 +11,12 @@ Category.init(
     primaryKey: true,
     autoIncrement: true,
   },
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+
   {
     sequelize,
     timestamps: false,
