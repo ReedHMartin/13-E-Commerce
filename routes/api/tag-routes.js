361de98d6939c34a7a9602da4a170e76ee473b31
Reed Martin <reedhmartin@gmail.com>
2023-04-14 18:30:46 -0400

    Updated, modified, and created todos to help me complete the code.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,29 @@
+// Define the API routes for CRUD operations on tags.
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
+  // TODO: find all tags
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
+  // TODO: find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  // TODO: create a new tag
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  // TODO: update a tag's name by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  // TODO: delete a tag by its `id` value
 });
 
 module.exports = router;
