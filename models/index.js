ffca0e57bda438e66e75689eaeda62e3612ff64c
Reed Martin <reedhmartin@gmail.com>
2023-04-14 18:47:09 -0400

    added todos to index.js.

--- models/index.js
+++ models/index.js
@@ -4,12 +4,16 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// TODO: Create:
 // Products belongsTo Category
 
+// TODO: Create:
 // Categories have many Products
 
+// TODO: Create:
 // Products belongToMany Tags (through ProductTag)
 
+// TODO: Create:
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
